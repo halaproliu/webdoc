95906265d11f239df1e7e604f44dce77421b791c
liuwenjian <liuwenjian@bilibili.co>
2022-12-23 10:40:56 +0800

    新增算法

--- src/isBalanced.js
+++ src/isBalanced.js
@@ -1,3 +1,9 @@
+
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
 // 平衡二叉树
 function isBalanced(root) {
     if (!root) return true
--- src/isValidBST.js
+++ src/isValidBST.js
@@ -1,3 +1,9 @@
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
 // 验证二叉搜索树
 function isValidBST(root) {
     function helper(root, lower, upper) {
